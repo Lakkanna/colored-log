1d41860c317ef3e812afaaf53d1961520736131f
Hotelsoft User <user1@IL1025.local>
2018-08-30 23:57:34 +0530

    colors added

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# colored-log
-Prints colored console log messages for javascript
+# consolemessage for print log in console
+
+### Helps to easily identify the log.
+
+## include in you javascript file
+```
+
+var console = require('consolemessage');
+
+console.log('debug message', 'debug');
+console.log('info message', 'info');
+console.log('warning message', 'warning');
+console.log('error message', 'error');
+console.log('critical message', 'critical');
+consol.log('normal message');
+```
+
+
+#### @credits colors 
---
+++ index.js
@@ -0,0 +1,26 @@
+var colors = require('colors');
+// debug, info, warning, error, critical
+exports.log = function(message, flag) {
+    switch (flag) {
+        case 'success':
+            console.log(message.green);
+            break;
+        case 'debug':
+            console.log(message.grey);
+            break;
+        case 'info':
+            console.log(message.blue);
+            break;
+        case 'warning':
+            console.log(message.yellow);
+            break;
+        case 'error':
+            console.log(message.red);
+            break;
+        case 'critical':
+            console.log(message.red);
+            break;
+        default:
+            console.log(message);
+    }
+}
